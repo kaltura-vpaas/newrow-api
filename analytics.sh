63169207f06d71c1b107903a055c99efba22a7bc
Philip Futernik <philip.futernik@gmail.com>
2020-05-29 15:49:46 -0400

    Add instructions

--- analytics.sh
+++ analytics.sh
@@ -2,7 +2,7 @@
 # ----------
 # GET /analytics/sessions
 # https://smart.newrow.com/backend/api/analytics/sessions
-
+# *** populate room_id with valid value (this is a newrow room Id, so need to run rooms.sh first)
 apiJson=$(cat <<EOF
 {
 	"method": "GET",
