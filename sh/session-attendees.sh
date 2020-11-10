40867505351d8dc7677f08edd30ad568476708c4
Philip Futernik <philip.futernik@gmail.com>
2020-11-10 10:43:20 -0500

    new folder for shell scripts

---
+++ sh/session-attendees.sh
@@ -0,0 +1,18 @@
+# Newrow API
+# ----------
+# GET /analytics/session-attendees
+# https://smart.newrow.com/backend/api/analytics/session-attendees
+# *** populate session_id with valid value (this is a newrow session Id, so need to run sessions.sh first)
+apiJson=$(cat <<EOF
+{
+	"method": "GET",
+	"action": "analytics/session-attendees",
+	"params": {
+        "session_id": 7687842
+	}
+}
+EOF
+)
+
+# Call Newrow API
+./newrowkaf.sh "$apiJson"
